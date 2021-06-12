a1f8801b57488448916e55c9444eff992c6f3c1d
jmrrgncpz <paz.jesr@gmail.com>
2021-06-12 17:05:32 +0800

    improve condition selection ui

--- pages/Home/ConditionSelectionView.js
+++ pages/Home/ConditionSelectionView.js
@@ -1,27 +1,41 @@
-import { Layout, Text, Button, ButtonGroup } from "@ui-kitten/components";
-import React from "react";
+import {
+  Layout,
+  Text,
+  Button,
+  ButtonGroup,
+  StyleService,
+  useTheme,
+  Icon,
+} from "@ui-kitten/components";
+import React, { useState } from "react";
 import FloatingMonitor from "../../components/floating-monitor";
 
-const conditionButtons = [
-  {
-    text: "New",
-    status: "success",
-  },
-  {
-    text: "Used - Like New",
-    status: "info",
-  },
-  {
-    text: "Used - Good",
-    status: "warning",
-  },
-  {
-    text: "Used - Fair",
-    status: "danger",
-  },
-];
-
 const ConditionSelectionView = ({ navigation }) => {
+  const theme = useTheme();
+  const conditions = [
+    {
+      text: "New",
+      status: "success",
+      value: 0,
+    },
+    {
+      text: "Used - Like New",
+      status: "info",
+      value: 1,
+    },
+    {
+      text: "Used - Good",
+      status: "warning",
+      value: 2,
+    },
+    {
+      text: "Used - Fair",
+      status: "danger",
+      value: 3,
+    },
+  ];
+  const [selectedCondition, setSelectedCondition] = useState(conditions[0]);
+
   return (
     <Layout level="2" style={{ display: "flex", flex: 1, padding: 25 }}>
       <Layout style={{ flex: 1, backgroundColor: "transparent" }}>
@@ -30,7 +44,12 @@ const ConditionSelectionView = ({ navigation }) => {
         />
       </Layout>
       <Layout
-        style={{ display: "flex", flex: 1, backgroundColor: "transparent" }}
+        style={{
+          display: "flex",
+          flex: 1,
+          backgroundColor: "transparent",
+          marginBottom: 24,
+        }}
       >
         <Text category="h1" style={{ fontSize: 36, marginBottom: 24 }}>
           Condition
@@ -43,22 +62,48 @@ const ConditionSelectionView = ({ navigation }) => {
             justifyContent: "space-between",
           }}
         >
-          {conditionButtons.map((condition) => (
-            <Button
-              key={`btn-${condition.text}`}
-              size="giant"
-              style={{
-                  marginLeft: -26,
-                  marginRight: -26
-              }}
-              status={condition.status}
-              onPress={() => navigation.navigate("DetailsView")}
-            >
-              {condition.text}
-            </Button>
-          ))}
+          {conditions.map(({ text, status, value }) => {
+            const isConditionSelected = value == selectedCondition.value;
+
+            return (
+              <Button
+                key={`btn-${text}`}
+                style={{
+                  borderRadius: 12,
+                  elevation: isConditionSelected ? 3 : 0,
+                  borderWidth: 0,
+                  backgroundColor: isConditionSelected
+                    ? theme[`color-${status}-200`]
+                    : "transparent",
+                }}
+                onPress={() => setSelectedCondition(conditions[value])}
+              >
+                <Text
+                  style={{
+                    color: isConditionSelected
+                      ? theme[`color-${status}-700`]
+                      : theme[`color-primary-default`],
+                    fontSize: isConditionSelected ? 20 : 16,
+                  }}
+                >
+                  {text}
+                </Text>
+              </Button>
+            );
+          })}
         </Layout>
       </Layout>
+
+      <Button
+        style={{ marginTop: "auto" }}
+        size="giant"
+        onPress={() => navigation.navigate("DetailsView")}
+        accessoryRight={(props) => (
+          <Icon {...props} name="chevron-right-outline"></Icon>
+        )}
+      >
+        Next
+      </Button>
     </Layout>
   );
 };
