d9589aba6e173954dc73ddc2ed626e140756645a
jmrrgncpz <paz.jesr@gmail.com>
2021-06-12 20:41:11 +0800

    add basic snapshot test

--- pages/Home/CategorySelectionView.js
+++ pages/Home/CategorySelectionView.js
@@ -1,135 +1,138 @@
 import React, { useState } from "react";
 import { Layout, Text, Button, Icon, StyleService, useStyleSheet } from "@ui-kitten/components";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { ScrollView } from "react-native-gesture-handler";
-
-const categories = [
-  {
-    name: "Home and Garden",
-    subcategories: [
-      {
-        name: "Tools",
-        icon: "tools",
-      },
-      {
-        name: "Furniture",
-        icon: "couch",
-      },
-      {
-        name: "Garden",
-        icon: "seedling",
-      },
-      {
-        name: "Appliances",
-        icon: "tv",
-      },
-    ],
-  },
-  {
-    name: "Entertainment",
-    subcategories: [
-      {
-        name: "Video Games",
-        icon: "gamepad",
-      },
-      {
-        name: "Books, Movies, and Music",
-        icon: "book",
-      },
-    ],
-  },
-  {
-    name: "Clothing and Accessories",
-    subcategories: [
-      {
-        name: "Bags and Luggage",
-        icon: "suitcase-rolling",
-      },
-      {
-        name: "Women's Clothing & Shoes",
-        icon: "female",
-      },
-      {
-        name: "Men's Clothing & Shoes",
-        icon: "male",
-      },
-      {
-        name: "Jewelry & Accessories",
-        icon: ["far", "gem"],
-      },
-    ],
-  },
-  {
-    name: "Family",
-    subcategories: [
-      {
-        name: "Health and Beauty",
-        icon: "heartbeat",
-      },
-      {
-        name: "Pet Supplies",
-        icon: "paw",
-      },
-      {
-        name: "baby",
-        icon: "puzzle-piece",
-      },
-    ],
-  },
-  {
-    name: "Electronics",
-    subcategories: [
-      {
-        name: "Electronics & Computers",
-        icon: "laptop",
-      },
-      {
-        name: "Mobile Phones",
-        icon: "mobile-alt",
-      },
-    ],
-  },
-  {
-    name: "Hobbies",
-    subcategories: [
-      {
-        name: "Bicycles",
-        icon: "bicycle",
-      },
-      {
-        name: "Arts & Crafts",
-        icon: "paint-brush",
-      },
-      {
-        name: "Sports & Outdoors",
-        icon: "basketball-ball",
-      },
-      {
-        name: "Auto Parts",
-        icon: "car",
-      },
-      {
-        name: "Musical Instruments",
-        icon: "guitar",
-      },
-      {
-        name: "Antiques & Collectibles",
-        icon: "camera-retro",
-      },
-    ],
-  },
-  {
-    name: "Others",
-    subcategories: [
-      {
-        name: "Others",
-        icon: "cube",
-      },
-    ],
-  },
-];
-
+import { ScrollView } from 'react-native';
 const CategorySelectionView = ({ navigation }) => {
+  const categories = [
+    {
+      name: "Home and Garden",
+      subcategories: [
+        {
+          name: "Tools",
+          icon: "tools",
+        },
+        {
+          name: "Furniture",
+          icon: "couch",
+        },
+        {
+          name: "Garden",
+          icon: "seedling",
+        },
+        {
+          name: "Appliances",
+          icon: "tv",
+        },
+      ],
+    },
+    {
+      name: "Entertainment",
+      subcategories: [
+        {
+          name: "Video Games",
+          icon: "gamepad",
+        },
+        {
+          name: "Books, Movies, and Music",
+          icon: "book",
+        },
+      ],
+    },
+    {
+      name: "Clothing and Accessories",
+      subcategories: [
+        {
+          name: "Bags and Luggage",
+          icon: "suitcase-rolling",
+        },
+        {
+          name: "Women's Clothing & Shoes",
+          icon: "female",
+        },
+        {
+          name: "Men's Clothing & Shoes",
+          icon: "male",
+        },
+        {
+          name: "Jewelry & Accessories",
+          icon: ["far", "gem"],
+        },
+      ],
+    },
+    {
+      name: "Family",
+      subcategories: [
+        {
+          name: "Health and Beauty",
+          icon: "heartbeat",
+        },
+        {
+          name: "Pet Supplies",
+          icon: "paw",
+        },
+        {
+          name: "Baby & Kids",
+          icon: "baby",
+        },
+        {
+          name: 'Toys & Games',
+          icon: "puzzle-piece"
+        }
+      ],
+    },
+    {
+      name: "Electronics",
+      subcategories: [
+        {
+          name: "Electronics & Computers",
+          icon: "laptop",
+        },
+        {
+          name: "Mobile Phones",
+          icon: "mobile-alt",
+        },
+      ],
+    },
+    {
+      name: "Hobbies",
+      subcategories: [
+        {
+          name: "Bicycles",
+          icon: "bicycle",
+        },
+        {
+          name: "Arts & Crafts",
+          icon: "paint-brush",
+        },
+        {
+          name: "Sports & Outdoors",
+          icon: "basketball-ball",
+        },
+        {
+          name: "Auto Parts",
+          icon: "car",
+        },
+        {
+          name: "Musical Instruments",
+          icon: "guitar",
+        },
+        {
+          name: "Antiques & Collectibles",
+          icon: "camera-retro",
+        },
+      ],
+    },
+    {
+      name: "Others",
+      subcategories: [
+        {
+          name: "Others",
+          icon: "cube",
+        },
+      ],
+    },
+  ];
+
   const [selectedCategory, setSelectedCategory] = useState("");
   const styles = useStyleSheet(themedStyles)
 
@@ -137,14 +140,6 @@ const CategorySelectionView = ({ navigation }) => {
     <Button
       class="subcategory"
       style={{
-        boxSizing: "border-box",
-        width: "auto",
-        height: 55,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        padding: 15,
         shadowColor: 'color-info-default',
         elevation: 3,
         shadowOffset: { width: 5, height: 5 },
@@ -153,12 +148,10 @@ const CategorySelectionView = ({ navigation }) => {
         backgroundColor: "#ffffff",
         overflow: "visible",
         borderRadius: 12,
+        borderWidth: 0,
         marginRight: 12,
-        borderWidth: 0
       }}
-      onPress={() => {
-        navigation.navigate('ConditionSelectionView')
-      }}
+      onPress={() => navigation.navigate('ConditionSelectionView')}
       accessoryLeft={() => (
         <FontAwesomeIcon
           color="rgb(2, 152, 239)"
@@ -176,17 +169,19 @@ const CategorySelectionView = ({ navigation }) => {
     <Layout
       id="category-selection-view"
       level="2"
-      style={{ display: "flex", flex: 1, padding: 25, paddingRight: 0 }}
+      style={{ display: "flex", flex: 1 }}
     >
       <ScrollView
         showsVerticalScrollIndicator={false}
-        style={{ overflow: "visible" }}
+        contentContainerStyle={{
+          paddingVertical: 25
+        }}
       >
         <Text
           category="h1"
           style={{
-            fontSize: 36,
-            marginBottom: 36,
+            marginBottom: 24,
+            marginLeft: 25
           }}
         >
           Category
@@ -202,8 +197,8 @@ const CategorySelectionView = ({ navigation }) => {
             <Text
               category="h2"
               style={{
-                fontSize: 24,
                 marginBottom: 8,
+                marginLeft: 25
               }}
             >
               {category.name}
@@ -212,8 +207,8 @@ const CategorySelectionView = ({ navigation }) => {
               contentContainerStyle={{
                 justifyContent: "flex-start",
                 alignItems: "center",
-                paddingVertical: 10,
-                paddingHorizontal: 5,
+                paddingVertical: 5,
+                paddingHorizontal: 25
               }}
               showsHorizontalScrollIndicator={false}
               horizontal={true}
@@ -237,8 +232,6 @@ const CategorySelectionView = ({ navigation }) => {
 export default CategorySelectionView;
 
 const themedStyles = StyleService.create({
-  subcategory: {
-  },
   icon: {
     marginRight: 12
   }
