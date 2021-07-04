904ea23d7712e7877bddcdb2b277a849abc86b7e
jmrrgncpz <paz.jesr@gmail.com>
2021-07-04 17:09:32 +0800

    implement explore view of Item component

--- components/item.js
+++ components/item.js
@@ -3,6 +3,9 @@ import { Text, StyleService, useStyleSheet } from "@ui-kitten/components";
 import { Image, View, TouchableOpacity } from "react-native";
 import Tag from "./tag";
 import { getItemFirstImageURLAsync } from "../services/ItemService";
+import { TouchableNativeFeedback } from "react-native-gesture-handler";
+import useWindowDimensions from "react-native/Libraries/Utilities/useWindowDimensions";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Item({
   name,
@@ -17,55 +20,122 @@ export default function Item({
   // will come from newly added item
   uploadTask,
   images,
+  variant = "default", // default | explore | view | select | offered
 }) {
-  const styles = useStyleSheet(stylesheet);
+  const window = useWindowDimensions();
   const [imageURL, setImageURL] = useState(null);
+  const sharedStyles = useStyleSheet(sharedStyleSheet);
   useEffect(() => {
     (async () => {
       const firstImageURL = await getItemFirstImageURLAsync(id);
       setImageURL(firstImageURL);
     })();
   }, [id]);
-  return (
-    <TouchableOpacity style={[customStyle, styles.container]}>
-      <View
-        elevation={3}
-        style={{
-          flexDirection: "row",
-        }}
-      >
-        {uploadTask ? (
-          <Image style={styles.image} source={{ uri: images[0].uri }} />
-        ) : (
-          <Image style={styles.image} source={{ uri: imageURL }} />
-        )}
 
-        <View style={styles.details}>
-          <View style={styles.itemHeader}>
-            <View style={styles.headerLeft}>
-              <Text category="h3">{name}</Text>
-              <Text category="c1" style={styles.category}>
-                {category.name}
-              </Text>
+  switch (variant) {
+    case "default":
+      const defaultStyles = useStyleSheet(defaultStyleSheet);
+      return (
+        <TouchableOpacity style={[customStyle, defaultStyles.container]}>
+          <View
+            elevation={3}
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            {uploadTask ? (
+              <Image
+                style={defaultStyles.image}
+                source={{ uri: images[0].uri }}
+              />
+            ) : (
+              <Image style={defaultStyles.image} source={{ uri: imageURL }} />
+            )}
+
+            <View style={defaultStyles.details}>
+              <View style={defaultStyles.itemHeader}>
+                <View style={defaultStyles.headerLeft}>
+                  <Text category="h3">{name}</Text>
+                  <Text category="c1" style={defaultStyles.category}>
+                    {category.name}
+                  </Text>
+                </View>
+                <Tag
+                  style={defaultStyles.condition}
+                  text={condition.text}
+                  status={condition.status}
+                />
+              </View>
+              <View style={sharedStyles.tagsContainer}>
+                {tags.map((tag, idx) => (
+                  <Tag key={idx} text={tag} style={defaultStyles.tag} />
+                ))}
+              </View>
             </View>
+          </View>
+        </TouchableOpacity>
+      );
+    case "explore":
+      const exploreStyles = useStyleSheet(exploreStyleSheet);
+      return (
+        <View
+          style={[
+            { width: window.width - 24, height: window.width - 24 },
+            exploreStyles.container,
+          ]}
+        >
+          <TouchableNativeFeedback style={exploreStyles.image}>
+            <Image
+              width={window.width - 24}
+              height={window.width - 24}
+              source={{ uri: imageURL }}
+              style={exploreStyles.image}
+            />
             <Tag
-              style={styles.condition}
+              style={exploreStyles.tag}
               text={condition.text}
               status={condition.status}
             />
-          </View>
-          <View style={styles.tagsContainer}>
-            {tags.map((tag, idx) => (
-              <Tag key={idx} text={tag} style={styles.tag} />
-            ))}
-          </View>
+            <LinearGradient
+              style={[
+                exploreStyles.overlay,
+                { width: window.width - 24, maxHeight: window.width / 2 },
+              ]}
+              locations={[0, 1]}
+              colors={["transparent", "rgba(0,0,0,0.7)"]}
+            >
+              <View style={exploreStyles.itemHeader}>
+                <Text category="h2" style={{ color: "white" }}>
+                  {name}
+                </Text>
+                <Text category="c1" style={{ color: "white" }}>
+                  {category.name}
+                </Text>
+              </View>
+              <View>
+                <Text
+                  category="p1"
+                  style={exploreStyles.description}
+                  numberOfLines={3}
+                  ellipsizeMode="tail"
+                >
+                  {description}
+                </Text>
+              </View>
+            </LinearGradient>
+          </TouchableNativeFeedback>
         </View>
-      </View>
-    </TouchableOpacity>
-  );
+      );
+  }
 }
 
-const stylesheet = StyleService.create({
+const sharedStyleSheet = StyleService.create({
+  tagsContainer: {
+    flexDirection: "row",
+  },
+});
+
+const defaultStyleSheet = StyleService.create({
   image: {
     width: 80,
     height: 80,
@@ -110,8 +180,29 @@ const stylesheet = StyleService.create({
     marginRight: 8,
     marginBottom: 8,
   },
-  tagsContainer: {
-    flexDirection: "row",
-  },
   descriptionContainer: {},
+});
+
+const exploreStyleSheet = StyleService.create({
+  container: {
+    marginBottom: 12,
+  },
+  overlay: {
+    bottom: 0,
+    padding: 12,
+    position: "absolute",
+    borderRadius: 8,
+  },
+  tag: {
+    position: "absolute",
+    top: 12,
+    right: 12,
+  },
+  itemHeader: {
+    marginBottom: 12,
+  },
+  image: {
+    borderRadius: 8,
+  },
+  description: { color: "white" },
 });
